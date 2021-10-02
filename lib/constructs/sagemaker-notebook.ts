382ecb3879b48cbcae361e218b809112dce6abc5
Christian Bonzelet <christian.bonzelet@gmail.com>
2021-10-02 23:52:00 +0200

    feat: automate provisioning of s3 bucket for sagemaker data analysis

--- lib/constructs/sagemaker-notebook.ts
+++ lib/constructs/sagemaker-notebook.ts
@@ -1,6 +1,7 @@
 import * as cdk from "@aws-cdk/core";
 import * as ec2 from "@aws-cdk/aws-ec2";
 import * as iam from "@aws-cdk/aws-iam";
+import * as s3 from "@aws-cdk/aws-s3";
 import * as sagemaker from "@aws-cdk/aws-sagemaker";
 
 export interface SageMakerNotebookProps {
@@ -14,6 +15,14 @@ export class SageMakerNotebook extends cdk.Construct {
 
   constructor(scope: cdk.Construct, id: string, props: SageMakerNotebookProps) {
     super(scope, id);
+
+    const bucket = new s3.Bucket(this, "data-analysis", {
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      autoDeleteObjects: true,
+      versioned: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
 
     const sagemakerExecutionRole = new iam.Role(this, "sagemaker-execution-role", {
       assumedBy: new iam.ServicePrincipal("sagemaker.amazonaws.com"),
@@ -30,12 +39,12 @@ export class SageMakerNotebook extends cdk.Construct {
           statements: [
             new iam.PolicyStatement({
               effect: iam.Effect.ALLOW,
-              resources: ["arn:aws:s3:::SageMaker"],
+              resources: [bucket.bucketArn],
               actions: ["s3:ListBucket"],
             }),
             new iam.PolicyStatement({
               effect: iam.Effect.ALLOW,
-              resources: ["arn:aws:s3:::SageMaker/*"],
+              resources: [`${bucket.bucketArn}/*`],
               actions: ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
             }),
           ],
